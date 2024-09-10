8f5267c4f8366e2f6a9839db83718e98ce7dce6a
zihaowu <zihao.wu@igg.com>
2024-09-10 17:48:53 +0800

    暂存笔记

--- bhakti/handler/dipamkara_handler.py
+++ bhakti/handler/dipamkara_handler.py
@@ -19,10 +19,53 @@ class DipamkaraHandler(PipelineStage):
 
 
 '''
+状态码：
+0000: create
+0001: find
+0010: update
+0011: remove
+
+0000/Exception
+0000/OK
+...
+
+0010:
+数据库在启动服务器时选择并创建，后续客户端发送指令并执行：
 {
-    'op': 'c',
+    'opt': 'create',
+    'cmd': 'create',
     'param': {
-        
+        'vector': '[1,0,1,0,1]',
+        'document': {...},
+        'indices': [..],
+        'cached': 0
     }
+} -> {
+    'state': 0000/Exception,
+    'message': '...',
+    'data': 0
+} or {
+    'state': 0000/OK,
+    'message': 'OK',
+    'data': 1
 }
+
+{
+    'opt': 'read',
+    'cmd': 'vector_query',
+    'param': {
+        'vector': '[1,0,1,0,1]',
+        'metric': Metric,
+        'top_k': 4
+    }
+} -> {
+    'state': 0000/Exception,
+    'message': '...',
+    'data': 0
+} or {
+    'state': 0000/OK,
+    'message': 'OK',
+    'data': [(...),(...)...]
+}
+...
 '''
