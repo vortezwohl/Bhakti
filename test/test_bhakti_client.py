8819c58b349e7b62a163a4ee29c980594c5a81fc
吴子豪 <vortezwohl@proton.me>
2024-09-14 19:42:13 +0800

    更新测试脚本

--- test/test_bhakti_client.py
+++ test/test_bhakti_client.py
@@ -3,25 +3,26 @@ import numpy as np
 from bhakti.client import BhaktiReactiveClient
 from bhakti.util.async_run import sync
 from bhakti.util.func_timer import time_kept
-from bhakti.util.async_repeat import async_repeat
+from bhakti.util.async_repeat import async_repeat, await_repeat
 from bhakti.database import Metric
 
 
 @async_repeat(1)
 async def operate():
-    client = BhaktiReactiveClient(timeout=1)
+    client = BhaktiReactiveClient(timeout=999999, buffer_size=100000)
     results = list()
-    query_vector = np.random.randn(5)
+    query_vector = np.random.randn(4096)
     # results.append(query_vector)
     # results.append(await client.create(vector=query_vector, document={'age': 31, 'gender': 'unknown'}))
     # results.append(await client.create_index(index='age'))
     # results.append(await client.create_index(index='gender'))
     # results.append(await client.remove_index(index='gender'))
-    results.append(await client.vector_query(vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=2))
-    # results.append(await client.find_documents_by_vector_indexed(query='age > 30 && gender != "female"', vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=2))
+    # results.append(await client.vector_query(vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=1))
+    # results.append(await client.find_documents_by_vector(vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=1))
+    # results.append(await client.find_documents_by_vector_indexed(query='age < 31 && gender != "female"', vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=1))
     # results.append((await client.insight())['cached_docs'])
     # results.append((await client.insight())['inverted_indices'])
-    # results.append((await client.indexed_remove('age != 0')))
+    results.append((await client.indexed_remove('age != 0')))
     return results
 
 
--- test/test_bootstrap.py
+++ test/test_bootstrap.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     logging.getLogger("bhakti").setLevel(logging.DEBUG)
     logging.getLogger("dipamkara").setLevel(logging.DEBUG)
     start_bhakti_server(
-        dimension=5,
+        dimension=4096,
         db_path=r"E:\Python\bhakti\test_archive",
         db_engine=DBEngine.DIPAMKARA,
         cached=False
