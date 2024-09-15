6e273a7c6ff91b7d24f1e1f8f6450a5b16dd4cca
吴子豪 <vortezwohl@proton.me>
2024-09-16 02:36:21 +0800

    更新测试脚本

--- test/test_bhakti_client.py
+++ test/test_bhakti_client.py
@@ -7,20 +7,21 @@ from bhakti.database import Metric
 
 @async_repeat(1)
 async def operate():
-    client = BhaktiClient()
+    client = BhaktiClient(eof=b'_wzh_', verbose=True)
     results = list()
     query_vector = np.random.randn(4096)
     # results.append(query_vector)
     # results.append(await client.create(vector=query_vector, document={'age': 31, 'gender': 'unknown'}))
     # results.append(await client.create_index(index='age'))
-    # results.append(await client.create_index(index='gender'))
+    # results.append(await client.create_index(index='gender', detailed=False))
+    # results.append(await client.remove_index(index='age'))
     # results.append(await client.remove_index(index='gender'))
     # results.append(await client.vector_query(vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=1))
-    # results.append(await client.find_documents_by_vector(vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=1))
-    # results.append(await client.find_documents_by_vector_indexed(query='age < 31 && gender != "female"', vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=1))
+    # results.append(await client.find_documents_by_vector(vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=0))
+    results.append(await client.find_documents_by_vector_indexed(query='age <= 31 && gender != "female"', vector=query_vector, metric=Metric.DEFAULT_METRIC, top_k=100))
     # results.append((await client.insight())['cached_docs'])
     # results.append((await client.insight())['inverted_indices'])
-    results.append((await client.indexed_remove('age != 0')))
+    # results.append((await client.indexed_remove('age != 0')))
     return results
 
 
--- test/test_bootstrap.py
+++ test/test_bootstrap.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     logging.getLogger("bhakti").setLevel(logging.DEBUG)
     logging.getLogger("dipamkara").setLevel(logging.DEBUG)
     BhaktiServer(
+        eof=b'_wzh_',
         dimension=4096,
         db_path=r"E:\Python\bhakti\test_archive",
         db_engine=DBEngine.DIPAMKARA,
