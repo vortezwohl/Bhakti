dab4f3c21d2bcc18dcac124629d24eb940645bf4
吴子豪 <vortezwohl@proton.me>
2024-09-16 03:36:42 +0800

    fix:修复从脚本启动读取EOF配置未解码为字节串的bug

--- bhakti/bootstrap/bhakti_server.py
+++ bhakti/bootstrap/bhakti_server.py
@@ -26,7 +26,7 @@ from bhakti.const import (
     UTF_8
 )
 
-__VERSION__ = "0.2.11"
+__VERSION__ = "0.2.12"
 __AUTHOR__ = "Vortez Wohl"
 log = logging.getLogger("bhakti")
 
@@ -110,7 +110,7 @@ def start_bhakti_server_shell(**kwargs):
         cached=kwargs['cached'],
         host=kwargs['host'],
         port=kwargs['port'],
-        eof=kwargs['eof'],
+        eof=kwargs['eof'].encode(UTF_8),
         timeout=kwargs['timeout'],
         buffer_size=kwargs['buffer_size'],
         verbose=kwargs['verbose']
