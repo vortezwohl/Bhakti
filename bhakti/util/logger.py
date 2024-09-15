bcc29901ba3623957db35c0811ae7d8a3f8bdb3b
吴子豪 <vortezwohl@proton.me>
2024-09-16 04:40:54 +0800

    解决日志重复打印的问题

--- bhakti/util/logger.py
+++ bhakti/util/logger.py
@@ -10,8 +10,3 @@ logger.addHandler(console_handler)
 
 logger = logging.getLogger('bhakti.client')
 logger.setLevel(logging.INFO)
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(name)s : %(message)s')
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
