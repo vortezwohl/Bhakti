9cbce135d89535a1efa23abe0dd690b268649828
吴子豪 <vortezwohl@proton.me>
2024-09-16 02:42:27 +0800

    更新测试脚本

--- test/test_bootstrap.py
+++ test/test_bootstrap.py
@@ -1,11 +1,7 @@
-import logging
-
 from bhakti import BhaktiServer
 from bhakti.database import DBEngine
 
 if __name__ == '__main__':
-    logging.getLogger("bhakti").setLevel(logging.DEBUG)
-    logging.getLogger("dipamkara").setLevel(logging.DEBUG)
     BhaktiServer(
         eof=b'_wzh_',
         dimension=4096,
