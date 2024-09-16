d0c49e3ac39431583be9415dcc3b42b527f5a178
吴子豪 <vortezwohl@proton.me>
2024-09-17 00:04:59 +0800

    调整测试脚本

--- test/test_bootstrap.py
+++ test/test_bootstrap.py
@@ -2,11 +2,17 @@ from bhakti import BhaktiServer
 from bhakti.database import DBEngine
 
 if __name__ == '__main__':
-    BhaktiServer(
-        eof=b'_wzh_',
-        dimension=4096,
-        db_path=r"E:\Python\bhakti\test_archive",
-        db_engine=DBEngine.DIPAMKARA,
-        cached=False,
-        verbose=False
-    ).run()
+    bhakti_server = BhaktiServer(
+        dimension=1024,  # required, only vectors with 1024 dimensions are acceptable
+        db_path=r'E:\Python\bhakti\test_archive',  # required, path where stores data, portable
+        db_engine=DBEngine.DIPAMKARA,  # optional, default to dipamkara
+        cached=False,  # optional, default to false
+        host='0.0.0.0',  # optional, default to 0.0.0.0
+        port=23860,  # optional, default to 23860
+        eof=b'<eof>',  # optional, default to <eof>
+        timeout=4.0,  # optional, default to 4.0 seconds
+        buffer_size=256,  # optional, default to 4.0 seconds
+        verbose=False  # optional, default to false
+    )
+    # run server
+    bhakti_server.run()
